1dcc0ba897c3552637d8c7e086fbfbae23c296a8
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-05-05 17:38:56 -0400

    a

---
+++ src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Olá Mundo";
+    }
+}
