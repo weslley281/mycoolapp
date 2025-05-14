99955bc4af6b17f5e75a4bef602a5aba5f8506c5
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-05-14 09:16:51 -0400

    a

---
+++ 01-spring-boo-overview/02-dev-tools-demo/src/main/java/com/__dev_tools_demo/__dev_tools_demo/Application.java
@@ -0,0 +1,13 @@
+package com.__dev_tools_demo.__dev_tools_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 01-spring-boo-overview/02-dev-tools-demo/src/test/java/com/__dev_tools_demo/__dev_tools_demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__dev_tools_demo.__dev_tools_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
